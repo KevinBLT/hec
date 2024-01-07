953483c012fdf6e3d13f13fd1e723e1e1edbeaac
Kevin Bulteel <kb@koalasoft.de>
2024-01-07 23:13:51 +0100

    ipdate component example

--- packages/@hec.js/ui/example/component/component.js
+++ packages/@hec.js/ui/example/component/component.js
@@ -4,19 +4,21 @@ component('my-counter', { count: 0 }, ({ count }) => {
 
   let isDown = false;
 
+  const mousedown = async () => {
+    isDown = true;
+
+    while (isDown) {
+      count(count() + 1);
+      await new Promise(r => setTimeout(r, 16.6));
+    }
+
+  }
+
   return templateByName(
     new URL('./component.html', import.meta.url), 
     {
       count,
-      mousedown: async () => {
-        isDown = true;
-
-        while (isDown) {
-          count(count() + 1);
-          await new Promise(r => setTimeout(r, 25));
-        }
-
-      },
+      mousedown,
       mouseup: () => isDown = false,
       color: count.map(e => '#' + e.toString(16).padStart(6, '0')),
       bg: count.map(e => '#' + (e ^ 0xFFFFFF).toString(16).padStart(6, '0')),
