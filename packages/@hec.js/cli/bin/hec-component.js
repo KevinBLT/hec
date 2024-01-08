1016370bb89e973036818fccf0ddf2cf55f0b7a4
Kevin Bulteel <kb@koalasoft.de>
2024-01-08 20:15:31 +0100

    cli: create component from cli

---
+++ packages/@hec.js/cli/bin/hec-component.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { mkdir, writeFile } from 'fs/promises';
+
+import args from 'args';
+import path from 'path';
+
+args.option(['r', 'root'],    'Root path for component"', './');
+args.option(['w', 'web-dir'], 'Include path for component"', './');
+
+const options  = args.parse(process.argv),
+      root     = options.root,
+      name     = process.argv.at(-1),
+      dir      = path.parse(path.join(root, name, name + '.js')).dir,
+      template = `
+        import { component, templateByName } from '@hec.js/ui';
+
+        component('${name}', {}, () => {
+
+          return templateByName('${name}')
+        });
+      `.replaceAll('        ', '');
+
+await mkdir(dir).catch(_ => null);
+
+await writeFile(path.join(dir, name + '.js'),   template);
+await writeFile(path.join(dir, name + '.css'),  '');
+await writeFile(path.join(dir, name + '.html'), '<link rel="stylesheet" type="text/css" href="' + path.join(options.w, name, name + '.css') + '" >');
--- packages/@hec.js/cli/bin/hec.js
+++ packages/@hec.js/cli/bin/hec.js
@@ -5,5 +5,6 @@ import args from 'args';
 args.command('favicon', 'Create favicon by providing an svg favicon image');
 args.command('icons', 'Create icons (png) by providing an svg image');
 args.command('font', 'Creates a css file with a font using icons of the input directory');
+args.command('component', 'Creates a hec ui component');
 
 args.parse(process.argv);
