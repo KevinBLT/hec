fff79dab69ed4908fbfaba6364be935b4c8fb44f
Kevin Bulteel <kb@koalasoft.de>
2024-03-06 09:36:27 +0100

    fix: routing

--- packages/@hec.js/ui/lib/src/routing.js
+++ packages/@hec.js/ui/lib/src/routing.js
@@ -52,9 +52,11 @@ export function addRoute(route) {
   if (parentPath) {
     path = path == '/' ? '' : path;
     path = parentPath.replaceAll(/[^\/a-zA-Z0-9]+$/gm, '') + path.replaceAll(/^[^\/a-zA-Z0-9]+/gm, '');
-  }  
+  }
 
-  route.pattern = new URLPattern({ pathname: path.replaceAll(/\/+/g, '/').replace(/\/$/m, '') });
+  path = path.replaceAll(/\/+/g, '/').replace(/\/$/m, '');
+
+  route.pattern = new URLPattern({ pathname: !path ? '/' : path });
     
   targetRoutes.push(route);
   targetRoutes.sort(routeCompare);
